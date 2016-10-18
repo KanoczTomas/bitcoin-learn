c929eb2e4b8e4437a46a2d430d6e8d9bb8def044
Tomas Kanocz <tk141a@att.com>
2016-10-18 19:39:41 +0200

    added python script to generate BTC address

---
+++ generate_address.py
@@ -0,0 +1,33 @@
+import ecdsa
+import base58
+import hashlib
+import os
+
+#private_key = os.urandom(32);
+private_key = "ce59a7b52814be59b483b25081ef448256e7acdaee6ae8b4b7674c78b1b8667c"
+
+signing_key = ecdsa.SigningKey.from_string(private_key.decode('hex'), curve=ecdsa.SECP256k1);
+verification_key = signing_key.get_verifying_key().to_string();
+
+def create_p2pkh_address(public_key, debug=False):
+  padded_pk = '\04' + public_key;
+  ripemd160=hashlib.new('ripemd160');
+  ripemd160.update(hashlib.sha256(padded_pk).digest())
+  hash_pk = ripemd160.digest()
+  padded_hash_pk = '\00' + hash_pk
+  checksum = hashlib.sha256(hashlib.sha256(padded_hash_pk).digest()).digest()[:4]
+  address_bin = padded_hash_pk + checksum
+  if(debug):
+    print "public key is: " + public_key.encode('hex')
+    print "after pading 0x04: " + padded_pk.encode('hex')
+    print "sha256 of padded public key: " + hashlib.sha256(padded_pk).hexdigest()
+    print "ripemd16 or sha256 of padded public key: " + hash_pk.encode('hex')
+    print "padding with 0x00 to create checksum: " + padded_hash_pk.encode('hex')
+    print "checksum hash = sha256(sha256(of above): " + hashlib.sha256(hashlib.sha256(padded_hash_pk).digest()).digest()
+    print "taking 4 bytes of checksum hash and concatenating to padded hash: " + address_bin
+    print "base58 encoding of binary address follows ... "
+
+  return base58.b58encode(address_bin)
+
+print "our address is: " + create_p2pkh_address(verification_key, debug=True)
+  
